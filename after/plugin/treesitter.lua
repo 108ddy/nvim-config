d93783126e0b6a6525a1acbf80eab960271d2c6e
108ddy <mgbemena900@gmail.com>
2026-03-06 11:44:08 +0200

    Added new languages to Treesitter plugin and fixed a typo.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,14 +1,24 @@
 require('nvim-treesitter.configs').setup {
     ensure_installed = {
-        'bash', 'c', 'cpp', 'css',
-        'dockerfile', 'gitignore', 'html',
-        'java', 'javascript', 'json', 'lua',
-        'python', 'rust', 'tsx', 'typescript',
+        'asm', 'bash', 'c', 'cmake', 'cpp', 'css',
+        'desktop', 'dockerfile', 'gitignore', 'graphql',
+        'html', 'htmldjango', 'http', 'java', 'javascript', 'json',
+        'lua', 'markdown', 'markdown_inline', 'python', 'query',
+        'rust', 'scss', 'sql', 'tmux', 'tsx', 'typescript', 'vim',
+        'vue', 'xml', 'yaml', 'zig',
     },
 
     highlight = {
         enable = true,
-        additonal_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = false,
+    },
+
+    incremental_selection = {
+        enable = true,
+    },
+
+    indent = {
+        enable = true,
     },
 }
 
