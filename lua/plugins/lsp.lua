0fe2e7dd41674c0b62bb67bcbbd1f413371fb5cf
108ddy <mgbemena900@gmail.com>
2026-03-15 23:54:16 +0200

    Added mason and other useful plugins to extend it.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,74 @@
 return {
     "neovim/nvim-lspconfig",
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {}, },
+        "mason-org/mason-lspconfig.nvim",
+
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+    },
 
     config = function()
+        local cmp = require("cmp")
+        local cmp_lsp = require("cmp_nvim_lsp")
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                end,
+            },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            }),
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+            }),
+        })
+
+        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = "buffer" }
+            },
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = "path" }
+            }, {
+                { name = "cmdline" }
+            }),
+            matching = { disallow_symbol_nonprefix_matching = false },
+        })
+
+        -- Set up lspconfig.
         local nvim_lsp = vim.lsp
         local servers = {
             "clangd", "ruff", "rust_analyzer", "ts_ls", "ty",
         }
 
+        local capabilities = cmp_lsp.default_capabilities()
         local opts = { noremap = true, silent = true }
 
         vim.diagnostic.config({ virtual_text = true })
@@ -38,8 +100,6 @@ return {
         local lsp_flags = { debounce_text_changes = 150, }
 
         nvim_lsp.config("lua_ls", {
-            on_attach = on_attach,
-            flags = lsp_flags,
             settings = {
                 Lua = {
                     runtime = { version = "LuaJIT", },
@@ -62,26 +122,30 @@ return {
                 },
             },
         })
-        nvim_lsp.enable("lua_ls")
 
         nvim_lsp.config("tailwindcss", {
-            on_atach = on_attach,
-            flags = lsp_flags,
             filetypes = {
                 "htmldjango", "html", "css", "less", "sass", "scss",
                 "javascript", "javascriptreact", "typescript",
                 "typescriptreact", "vue", "svelte",
             },
         })
-        nvim_lsp.enable("tailwindcss")
+
+        for _, new_server in ipairs({ "lua_ls", "tailwindcss" }) do
+            table.insert(servers, new_server)
+        end
 
         for _, lsp in ipairs(servers) do
             nvim_lsp.config(lsp, {
                 on_attach = on_attach,
                 flags = lsp_flags,
+                capabilities = capabilities,
             })
-            nvim_lsp.enable(lsp)
         end
+
+        require("mason-lspconfig").setup({
+            ensure_installed = servers,
+        })
     end
 }
 
